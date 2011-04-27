5b94500351acbf3c6b55288910e82bce4c90578d
Erik LaBianca <erik.labianca@gmail.com>
2011-04-27 01:29:59 -0400

    - fork to github and make egg-friendly    added rudimentary setup.py    adding .gitignore    move code into python_rest_client module with __init__.py

---
+++ README.rst
@@ -0,0 +1,9 @@
+Description
+A REST Client for use in python, using httplib2 and urllib2.
+
+Includes a version that is suitable for use in the Google App Engine environment.
+
+See httplib2 documentation here:
+
+Update 3 June 2008
+Added in HTTP Basic, Digest authentication (re-using code from httplib2) to the version of the client that can run in the Google App Engine environment.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='python-rest-client',
+    version='0.0.svn.13.1',
+    description='Python REST Client.',
+    long_description=open(os.path.join(ROOT, 'README.rst')).read(),
+    author="Ben O'Steen",
+    author_email='',
+    maintainer='Erik LaBianca',
+    maintainer_email='erik.labianca@gmail.com',
+    url='https://github.com/easel/python-rest-client',
+    license='FedoraCommons',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Fedora Commons',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
